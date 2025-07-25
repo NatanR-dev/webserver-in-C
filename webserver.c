f96bbc85da820e855ed71580d14d18a507bf0131
Natan Ribeiro <natanribeirodeveloper@gmail.com>
2025-07-25 11:55:22 -0300

    feat: add POSIX socket lib for other platforms (Linux/Mac)

--- webserver.c
+++ webserver.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #ifdef _WIN32
 #include <winsock2.h>
+typedef int socklen_t;
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #endif
 
 int main() {
