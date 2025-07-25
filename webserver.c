8fde98336cd8c49d36d5e2ec5494543170f330b1
Natan Ribeiro <natanribeirodeveloper@gmail.com>
2025-07-25 12:00:34 -0300

    feat: define port and create socket

--- webserver.c
+++ webserver.c
@@ -30,7 +30,16 @@ void close_connection(int clientConnection) {
     #endif
 }
 
+#define PORT 8080
+
 int main() {
+    init_sockets();
+
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serverAddress = {0};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
 
     return 0;
 }
