8de94e01bfdeeff8e09de8aecc36793a48cbdaea
Natan Ribeiro <natanribeirodeveloper@gmail.com>
2025-07-25 11:53:59 -0300

    feat: add WinSock lib for Windows

--- webserver.c
+++ webserver.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
-#include <unistd.h> 
 #ifdef _WIN32
-#include <windows.h> 
+#include <winsock2.h>
 #endif
 
 int main() {
-    #ifdef _WIN32
-    SetConsoleOutputCP(CP_UTF8); 
-    #endif
-
-    for (int i = 1; ; i++) {
-        printf("Receba-->\u26BD(%d)\n", i); 
-        sleep(1); 
-    }
 
     return 0;
 }
