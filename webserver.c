371e3994d0d8ba5a42a2c766e3049c2cbdbb6792
Natan Ribeiro <natanribeirodeveloper@gmail.com>
2025-07-28 15:26:08 -0300

    fix: validate number of routes

--- webserver.c
+++ webserver.c
@@ -54,6 +54,10 @@ void close_connection(int clientConnection) {
 }
 
 void addRoute(Server* server, char* path, RouteHandler handler) {
+    if (server->routeCount >= MAX_ROUTES) {
+        fprintf(stderr, "Error: Maximum number of routes (%d) exceeded\n", MAX_ROUTES);
+        return;
+    }
     server->routes[server->routeCount].path = strdup(path);
     server->routes[server->routeCount].handler = handler;
     server->routeCount++;
